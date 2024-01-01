13147ee82ec09afdc94dfddb209416074933009c
adityaS011 <adityasinghtech011@gmail.com>
2024-01-01 23:07:10 +0530

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// ... Existing imports
 
 function App() {
   const [cryptocurrencies, setCryptocurrencies] = useState([]);
